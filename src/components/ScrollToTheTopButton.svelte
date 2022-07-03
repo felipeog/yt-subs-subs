898d94846acab3b129c4dd6fdb3dde6ff58987da
felipeog <17603069+felipeog@users.noreply.github.com>
2022-07-03 19:38:21 -0300

    Add feat

---
+++ src/components/ScrollToTheTopButton.svelte
@@ -0,0 +1,28 @@
+<svelte:options immutable />
+
+<script>
+  import { customFade } from "../animations/customFade";
+
+  let scrollY;
+  let innerHeight;
+
+  $: shouldRender = scrollY > innerHeight;
+
+  function scrollToTheTop() {
+    document.body.scrollIntoView();
+  }
+</script>
+
+<svelte:window bind:scrollY bind:innerHeight />
+
+{#if shouldRender}
+  <button in:customFade on:click={scrollToTheTop}>to the top</button>
+{/if}
+
+<style>
+  button {
+    position: fixed;
+    bottom: 1rem;
+    right: 1rem;
+  }
+</style>
