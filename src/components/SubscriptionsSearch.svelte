a183d90483ff41cfac8d8a3435fdd4685a5d4c19
felipeog <17603069+felipeog@users.noreply.github.com>
2022-07-03 00:07:56 -0300

    Separate list rendering

--- src/components/SubscriptionsSearch.svelte
+++ src/components/SubscriptionsSearch.svelte
@@ -1,18 +1,32 @@
 <script>
   import SubscriptionsList from "./SubscriptionsList.svelte";
+  import ChannelsSearchList from "./ChannelsSearchList.svelte";
   import { createSubscriptionStore } from "../stores/subscription";
+  import { createChannelStore } from "../stores/channel";
 
   const { subscriptionStore, loadSubscriptions } = createSubscriptionStore();
+  const { channelStore, searchChannels } = createChannelStore();
 
   let headTitle = "yt-subs-subs";
-  let channelId = import.meta.env.DEV
+  let selectedChannel = {};
+  let query = import.meta.env.DEV
     ? import.meta.env.VITE_DEV_CHANNEL_ID ?? ""
     : "";
 
-  async function handleSubmit() {
-    await loadSubscriptions({ channelId });
+  $: isLoading = $subscriptionStore.loading || $channelStore.loading;
 
-    headTitle = `yt-subs-subs | channel id: ${channelId}`;
+  async function handleChannelsSearch() {
+    await searchChannels({ query });
+  }
+
+  function handleSubscriptionsLoad({ snippet }) {
+    return async function () {
+      await loadSubscriptions({ channelId: snippet.channelId });
+
+      selectedChannel = snippet;
+      query = snippet.channelTitle;
+      headTitle = `yt-subs-subs | ${snippet.channelTitle}`;
+    };
   }
 </script>
 
@@ -21,18 +35,25 @@
 </svelte:head>
 
 <section>
-  <form on:submit|preventDefault={handleSubmit}>
+  <form on:submit|preventDefault={handleChannelsSearch}>
     <input
-      bind:value={channelId}
-      placeholder="channel id"
-      disabled={$subscriptionStore.loading}
+      bind:value={query}
+      placeholder="channel name or id"
+      disabled={isLoading}
       type="text"
       required
     />
-    <button type="submit" disabled={$subscriptionStore.loading}>
-      get subs
-    </button>
+
+    <button type="submit" disabled={isLoading}>search</button>
   </form>
+
+  {#key $channelStore.channels}
+    <ChannelsSearchList
+      {...$channelStore}
+      {selectedChannel}
+      handleClick={handleSubscriptionsLoad}
+    />
+  {/key}
 
   {#key $subscriptionStore.subscriptions}
     <SubscriptionsList {...$subscriptionStore} />
@@ -44,7 +65,14 @@
     margin-top: 6rem;
   }
 
+  form {
+    display: flex;
+    gap: 0.6rem;
+    margin-bottom: 0.6rem;
+  }
+
   input {
+    flex-grow: 1;
     width: 18rem;
   }
 </style>
