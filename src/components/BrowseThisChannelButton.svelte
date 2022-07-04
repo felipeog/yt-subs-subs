e4e5deff24b2ba35df168ec6be12d4e8476b1513
felipeog <17603069+felipeog@users.noreply.github.com>
2022-07-04 00:12:04 -0300

    Add browse button

---
+++ src/components/BrowseThisChannelButton.svelte
@@ -0,0 +1,18 @@
+<svelte:options immutable />
+
+<!-- TODO: scroll to the top -->
+<script>
+  import { loadSubscriptions } from "../stores/subscriptions";
+
+  export let channelId;
+
+  async function handleClick() {
+    await loadSubscriptions({ channelId });
+  }
+</script>
+
+<button class="small" on:click={handleClick}>browse this channel</button>
+
+<style>
+  /* TODO: create smaller button */
+</style>
