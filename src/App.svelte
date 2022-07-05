48f77fc676b1a023aa13b8831c81d9772a8d405b
felipeog <17603069+felipeog@users.noreply.github.com>
2022-07-05 01:03:59 -0300

    Smooth list height change

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,26 @@
 <script>
+  import { tweened } from "svelte/motion";
+
   import Header from "./components/Header.svelte";
   import Search from "./components/Search.svelte";
   import ScrollToTheTopButton from "./components/ScrollToTheTopButton.svelte";
+
+  const wrapperHeight = tweened(undefined, {
+    duration: 200,
+  });
+
+  let clientHeight;
+
+  $: $wrapperHeight = clientHeight;
 </script>
 
-<main>
-  <Header />
-  <Search />
-  <ScrollToTheTopButton />
-</main>
+<div style="height: {$wrapperHeight}px;">
+  <main bind:clientHeight>
+    <Header />
+    <Search />
+    <ScrollToTheTopButton />
+  </main>
+</div>
 
 <style>
   main {
