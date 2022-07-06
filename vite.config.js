24872a2d64ae5577ac7cd03cd44502fd3b4feea5
felipeog <17603069+felipeog@users.noreply.github.com>
2022-07-06 01:45:46 -0300

    Add aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
+import path from "path";
+import fs from "fs";
+
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+const srcPath = path.resolve("./src");
+const directoryEntries = fs.readdirSync(srcPath, { withFileTypes: true });
+const absolutePathAliases = directoryEntries.reduce((acc, directoryEntry) => {
+  if (directoryEntry.isDirectory()) {
+    return {
+      ...acc,
+      [directoryEntry.name]: path.join(srcPath, directoryEntry.name),
+    };
+  }
+
+  return acc;
+}, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: absolutePathAliases,
+  },
 });
